e422b3311fee984b8937acd2f08bff88bd2ee6f1
张誉川 <zhangyuchuan@evercreative.com.cn>
2019-11-13 15:02:49 +0800

    eureka feign负载均衡

--- eureka-consumer-feign/src/main/java/com/achan/ApplicationConsumerFeign.java
+++ eureka-consumer-feign/src/main/java/com/achan/ApplicationConsumerFeign.java
@@ -3,21 +3,21 @@ package com.achan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ApplicationConsumer2 {
+public class ApplicationConsumerFeign {
 
     @Bean
-    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ApplicationConsumer2.class).web(true).run(args);
+        new SpringApplicationBuilder(ApplicationConsumerFeign.class).web(true).run(args);
     }
 }
---
+++ eureka-consumer-feign/src/main/java/com/achan/controller/DcController.java
@@ -0,0 +1,23 @@
+package com.achan.controller;
+
+import com.achan.DcClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class DcController {
+
+    @Autowired
+    DcClient dcClient;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        return dcClient.consumer();
+    }
+
+}
