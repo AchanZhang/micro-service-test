58f47b436ed21b4fc0f3cd35e19bb6ff97251896
张誉川 <zhangyuchuan@evercreative.com.cn>
2019-11-13 14:21:18 +0800

    eureka Ribbon

---
+++ eureka-consumer-ribbon/src/main/java/com/achan/ApplicationConsumer2.java
@@ -0,0 +1,23 @@
+package com.achan;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApplicationConsumer2 {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ApplicationConsumer2.class).web(true).run(args);
+    }
+}
