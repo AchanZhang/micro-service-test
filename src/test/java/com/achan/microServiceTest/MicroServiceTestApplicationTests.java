e2fb3c112417eaeaef251b106c37b5d307de6d66
张誉川 <zhangyuchuan@evercreative.com.cn>
2019-11-13 09:07:01 +0800

    first commit

---
+++ src/test/java/com/achan/microServiceTest/MicroServiceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achan.microServiceTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
