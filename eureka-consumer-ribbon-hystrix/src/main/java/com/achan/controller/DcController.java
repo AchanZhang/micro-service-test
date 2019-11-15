e2117fb8973a8a885853aa57009575fae3abed39
张誉川 <zhangyuchuan@evercreative.com.cn>
2019-11-15 11:36:03 +0800

    服务容错保护（Hystrix服务降级）

---
+++ eureka-consumer-ribbon-hystrix/src/main/java/com/achan/controller/DcController.java
@@ -0,0 +1,23 @@
+package com.achan.controller;
+
+import com.achan.service.ConsumerService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class DcController {
+
+    @Autowired
+    ConsumerService consumerService;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        return consumerService.consumer();
+    }
+
+}
