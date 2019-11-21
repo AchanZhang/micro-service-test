de1f4ca5f24d85df570b883120b8938cfe710b42
张誉川 <zhangyuchuan@evercreative.com.cn>
2019-11-21 10:24:45 +0800

    过滤器 ZuulFilter

--- api-gateway/src/main/java/com/achan/GatewayApplication.java
+++ api-gateway/src/main/java/com/achan/GatewayApplication.java
@@ -1,14 +1,24 @@
 package com.achan;
 
+import com.achan.filter.AccessFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScanPackages;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
-@SpringCloudApplication
+@SpringBootApplication(scanBasePackages = "com.achan")
 public class GatewayApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(GatewayApplication.class).web(true).run(args);
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
     }
 }
